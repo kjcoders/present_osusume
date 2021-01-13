ae24e8cc8de1e11d8576edd717a6bac08686b666
Minjae <sochubert@gmail.com>
2021-01-13 21:27:13 +0900

    Start Page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
-import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
     </BrowserRouter>
